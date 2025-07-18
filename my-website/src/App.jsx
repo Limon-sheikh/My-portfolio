432832f56bbaa323ef677c60bd83553b3063fe39
Limon sheikh <cmtlover111@gmail.com>
2025-07-18 17:15:08 +0600

    setup

---
+++ my-website/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Home from '../src/pages/Home'
+
+function App() {
+
+  return (
+    <div >
+      <Home></Home>
+    </div>
+  )
+}
+
+export default App
---
+++ my-website/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+const Home = () => {
+    return (
+        <header>
+            
+        </header>
+    );
+};
+
+export default Home;
