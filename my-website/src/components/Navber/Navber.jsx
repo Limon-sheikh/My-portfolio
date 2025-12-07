de5e06382f23097f876c37d6a320ce21ea67b737
Limon Sheikh <cmtlover111@gmail.com>
2025-12-07 11:04:44 +0600

    some change

--- my-website/src/components/Navber/Navber.jsx
+++ my-website/src/components/Navber/Navber.jsx
@@ -9,43 +9,84 @@ const Navbar = () => {
   const [open, setOpen] = useState(false);
   const toggle = () => setOpen(!open);
 
+  // mobile device navber show and scrolling stop hendeler
   useEffect(() => {
     if (open) {
-      document.body.style.overflow = "hidden";  // ✅ scroll বন্ধ
+      document.body.style.overflow = "hidden"; // ✅ scroll বন্ধ
     } else {
-      document.body.style.overflow = "auto";    // ✅ scroll চালু
+      document.body.style.overflow = "auto"; // ✅ scroll চালু
     }
-  
+
     return () => {
-      document.body.style.overflow = "auto";    // ✅ safety reset
+      document.body.style.overflow = "auto"; // ✅ safety reset
     };
   }, [open]);
+
+
+  // navber sticky state hendeler
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 90) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   
 
   return (
     <>
       {/* Desctop menu */}
-      <header className="w-full h-20 bg-red-500 flex items-center z-50 shadow-lg ">
-        <nav className="container mx-auto bg-green-400 px-5 md:px-0 h-16 flex items-center justify-between">
-          <Logo/>
-          <NavLinks className="hidden md:block"/>
-          <MenuToggle isOpen={open} toggle={toggle} />
-          <Button title='hire me' border={false} className="text-2xl text-gray rounded-md md:px-4 md:py-2 xl:px-5 xl:py-3 capitalize font bg-accent hover:bg-hover transition-all duration-150 ease-linear hidden md:block"/>
+      <header className="w-full h-20 flex items-center z-50 shadow-lg ">
+        <nav
+          className={`w-full bg-white z-50 md:px-0 h-20 ${isSticky ? "fixed top-0 " : "relative "}`}>
+          <div className="container mx-auto h-20 flex items-center justify-between bg-fuchsia-600">
+            <Logo />
+            <NavLinks className="hidden md:block" />
+            <MenuToggle isOpen={open} toggle={toggle} />
+            <Button title="hire me" border={false}
+              className="text-2xl text-gray rounded-md md:px-4 md:py-2 xl:px-5 xl:py-3 capitalize font bg-accent hover:bg-hover transition-all duration-150 ease-linear hidden md:block"
+            />
+          </div>
         </nav>
       </header>
 
       {/* Mobile Menu Drawer */}
-      <div className={`fixed top-0 right-0 h-full w-[60vw] shadow-xl z-[70] border-l border-white/20 backdrop-blur-md bg-white/30 md:hidden transform transition-transform duration-300 ease-in-out ${open ? "translate-x-0" :  "translate-x-full" } flex flex-col justify-around`}>
+      <div
+        className={`fixed top-0 left-0 h-full w-[60vw] shadow-xl z-[70] border-l border-white/20 backdrop-blur-md bg-white/30 md:hidden transform transition-transform duration-300 ease-in-out ${
+          open ? "translate-x-0" : "-translate-x-full"
+        } flex flex-col justify-around`}
+      >
         <nav className=" ">
-          <NavLinks onLinkClick={toggle} className="md:hidden flex flex-col gap-3 items-center"/>
+          <NavLinks
+            onLinkClick={toggle}
+            className="md:hidden flex flex-col gap-3 items-center"
+          />
         </nav>
         <div className="py-2 flex justify-around items-center">
-          <Button onLinkClick={toggle} title='hire me' border={false} className="text-xl font-semibold text-gray capitalize rounded-md px-4 py-1 bg-accent hover:bg-hover transition-all duration-150 ease-linear"/>
+          <Button
+            onLinkClick={toggle}
+            title="hire me"
+            border={false}
+            className="text-xl font-semibold text-gray capitalize rounded-md px-4 py-1 bg-accent hover:bg-hover transition-all duration-150 ease-linear"
+          />
         </div>
       </div>
 
       {/* Backdrop Layer */}
-      {open && (<div onClick={toggle} className="fixed inset-0 md:hidden bg-black/10 backdrop-blur-sm z-[60] transition-opacity duration-300"></div>)}
+      {open && (
+        <div
+          onClick={toggle}
+          className="fixed inset-0 md:hidden bg-black/10 backdrop-blur-sm z-[60] transition-opacity duration-300"
+        ></div>
+      )}
     </>
   );
 };
