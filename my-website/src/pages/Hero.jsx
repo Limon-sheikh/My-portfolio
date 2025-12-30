bf255479ecfe0912a43b7e75b473403b4f147a37
Limon Sheikh <cmtlover111@gmail.com>
2025-12-30 22:51:55 +0600

    some change

--- my-website/src/pages/Hero.jsx
+++ my-website/src/pages/Hero.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       <section id="hero" className="min-h-[calc(100vh-80px)] md:flex md:items-center bg-sec_one_bg_c">
         <div className="container mx-auto">
           <div id="wraper" className="px-5 md:px-0 md:grid md:grid-cols-12 lg:gap-2">
-            <div className="pt-[4vh] md:pt-0 flex flex-col md:col-span-6">
+            <div className="pt-[2vh] sm:pt-[3vh] md:pt-0 flex flex-col md:col-span-6">
               <div className="">
                 <h1 className="text-[13vw] mt-8 sm:text-[10vw] md:text-6xl lg:text-7xl font-bold leading-tight ">
                   {hello} <br />
@@ -22,7 +22,7 @@ const Home = () => {
                 </h1>
                 {/* typewriter */}
                 <div className="mt-[9vh] flex items-center flex-wrap gap-1 py-1">
-                  <span className="text-sm text-secondary/60 font-medium">{title}</span>
+                  <span className="text-sm text-primary/60 font-medium">{title}</span>
                   <TypeWriter texts= {['front end developer', 'UI/UX designer', 'wordpress developer']}
                     typingSpeed={80} deletingSpeed={50} endDelay={500} startDelay={1000}
                   />
@@ -37,13 +37,11 @@ const Home = () => {
                   className={`border p-2 rounded-tl-3xl rounded-tr-3xl rounded-br-3xl`}
                 />
                 <div className="flex justify-between gap-4 md:justify-start md:gap-9">
-                  <Button title="view more" border={false}
-                    className="bg-accent text-gray px-7 py-2 font-semibold text-[4vw] sm:text-[3vw] md:text-xl hover:bg-hover
-                      transition-all duration-150 ease-linear rounded-md text-text_C_W"
+                  <Button title="view more" border={false} className="bg-accent px-7 py-2 font-semibold text-[4vw] 
+                    sm:text-[3vw] md:text-lg hover:bg-hover transition-all duration-300 ease-linear rounded-md text-text_C_W"
                   />
-                  <Button title="Downlod Cv" border={true}
-                    className="border-accent text-gray px-7 py-2 font-semibold text-[4vw] sm:text-[3vw] md:text-xl hover:bg-accent
-                    transition-all duration-150 ease-linear border-2 rounded-md"
+                  <Button title="Downlod Cv" border={true} className="border-accent text-primary px-7 py-2 font-semibold text-[4vw] 
+                    sm:text-[3vw] md:text-lg hover:bg-accent transition-all duration-300 ease-linear border-2 rounded-md"
                   />
                 </div>
               </div>
